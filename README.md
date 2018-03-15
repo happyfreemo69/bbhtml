4123da5242f97abbcb786ac75be9b9b08492533c
mpech <mpech404@gmail.com>
2018-03-16 00:34:29 +0100

    url,color

--- README.md
+++ README.md
@@ -1,7 +1,57 @@
 another parser
 
-this one covered with tests
+this one handling nested lists
 
-and handling nested lists
+covers:
+    
+    $grep 'test(' tests/lib/parser.js
+    test('[u]g[/u]', '<u>g</u>');
+    test('[i]g[/i]', '<i>g</i>');
+    test('[b]g[/b]', '<b>g</b>');
+    test('[p]g[/p]', '<p>g</p>');
+    test('[color=red]t[/color]', '<span style="color:red;">t</span>');
+    test('[color=#eeeeee]t[/color]', '<span style="color:#eeeeee;">t</span>');
+    test('[url=http://xx]t[/url]', '<a href="http://xx">t</a>');
+    test('[e]g[/e]', '[e]g[/e]');
+    test('d[u]g[b]test[/b]gro[/u]c', 'd<u>g<b>test</b>gro</u>c');
+    test('d[u]g[b]te[i]i[/i]st[/b]gro[/u]c', 'd<u>g<b>te<i>i</i>st</b>gro</u>c');
+    test('d[u]g[b]te[u]i[/u]st[/b]gro[/u]c', 'd<u>g<b>te<u>i</u>st</b>gro</u>c');
+    test('[list][*]mok\n[*]dobbles[/list]', '<ul><li>mok</li><li>dobbles</li></ul>',p);
+    test('[list][*]te[list][*]ok[/list]st[*]dobbles[/list]', '<ul><li>te<ul><li>ok</li></ul>st</li><li>dobbles</li></ul>',p);
+    test('[u]<li>test</li>[/u]', '<u>litest/li</u>');
 
+note:
 
+    does NOT cover bbcode of the form
+    
+    [*]cc\n
+    [*]cc\n
+
+    (idem not wrapped by [/?list])
+
+usage:
+
+    var p = new Parser();
+    try{
+        var html = p.parse('thebbcodestring')
+    }catch(e){
+        //errors may be raised, some because I should learn how to code, some you may want to hinder by customizing the parser
+    }
+
+customization:
+
+    Parser accepts {escapeHtml:str=>str} (this one would not escape anything..)
+    Parser::parse will not escape any not configured tag
+    you can add your own tag by modifying parser.nodes
+    var parser = new Parser
+    parser.nodes.url = this.nodeFactory.makeConstructor({
+        validTag: function(){
+            if(typeof(this.tag.attr)!='string'){
+                throw 'expect link for url';
+            }
+        },
+        html:function(){
+            var href = parser.escapeHtml(this.tag.attr);
+            return '<a href="'+href+'">'+this.nodes.map(node=>node.html()).join('')+'</a>';
+        }
+    })
--- tests/lib/parser.js
+++ tests/lib/parser.js
@@ -1,8 +1,8 @@
 var Parser = require('../../lib/parser');
 var assert = require('assert');
 describe('parser', function(){
-    var test = function(x,y){
-        var p = new Parser();
+    var test = function(x,y,parser){
+        var p = parser||new Parser();
         assert.equal(p.parse(x),y)
     }
     it('base', function(){
@@ -10,6 +10,9 @@ describe('parser', function(){
         test('[i]g[/i]', '<i>g</i>');
         test('[b]g[/b]', '<b>g</b>');
         test('[p]g[/p]', '<p>g</p>');
+        test('[color=red]t[/color]', '<span style="color:red;">t</span>');
+        test('[color=#eeeeee]t[/color]', '<span style="color:#eeeeee;">t</span>');
+        test('[url=http://xx]t[/url]', '<a href="http://xx">t</a>');
     });
     it('ignores not known tags', function(){
         test('[e]g[/e]', '[e]g[/e]');
@@ -28,7 +31,7 @@ describe('parser', function(){
         var Tag = Parser.Tag;
         p.nodes.list = p.nodeFactory.makeConstructor({alias:'ul'});
         p.nodes['*'] = p.nodeFactory.makeConstructor({alias:'li', crossCheck:false});
-        assert.equal(p.parse('[list][*]test\n[*]dobbles[/list]'), '<ul><li>test</li><li>dobbles</li></ul>');
+        test('[list][*]mok\n[*]dobbles[/list]', '<ul><li>mok</li><li>dobbles</li></ul>',p);
     });
 
     it('handles nested list', function(){
@@ -36,7 +39,7 @@ describe('parser', function(){
         var Tag = Parser.Tag;
         p.nodes.list = p.nodeFactory.makeConstructor({alias:'ul', nesting:'*'});
         p.nodes['*'] = p.nodeFactory.makeConstructor({alias:'li', crossCheck:false});
-        assert.equal(p.parse('[list][*]te[list][*]ok[/list]st[*]dobbles[/list]'), '<ul><li>te<ul><li>ok</li></ul>st</li><li>dobbles</li></ul>');
+        test('[list][*]te[list][*]ok[/list]st[*]dobbles[/list]', '<ul><li>te<ul><li>ok</li></ul>st</li><li>dobbles</li></ul>',p);
     });
     it('escapes html', function(){
         test('[u]<li>test</li>[/u]', '<u>litest/li</u>');
@@ -62,6 +65,7 @@ describe('parser', function(){
         var p = new Parser();
         var Tag = Parser.Tag;
         p.nodes.url = p.nodeFactory.makeConstructor({html:function(){
+            //yes injection on tag
             return '<a href="'+this.tag.attr+'">'+this.nodes.map(x=>x.html()).join('')+'</a>';
         }});
         assert.equal(p.parse('[url=http://ok]te[i]i[/i]st[/url]'),'<a href="http://ok">te<i>i</i>st</a>')
